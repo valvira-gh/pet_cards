af08c6cc3a77779cbf1038be5bf420dfefbeb695
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-05-29 17:13:24 +0300

    Used children-object to create model of both log-sites using dynamic reiting.

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+// deps
+import { useFormState } from "react-dom";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+// shadcn-ui + my components
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import React from "react";
+
+// DYNCAMIC ROUTES //
+const initialState = {
+  email: "",
+  password: "",
+};
+
+const handleAction = async (formState = initialState) => {
+  console.log(formState);
+  return formState;
+};
+
+const LogForm = ({ children }: { children: React.ReactNode }) => {
+  const [formState, formAction] = useFormState(handleAction, initialState);
+
+  return (
+    <form action={formAction}>
+      <CardHeader>
+        <CardTitle>{children} lomake</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <Input placeholder="Email" />
+        <Input placeholder="Password" />
+      </CardContent>
+      <CardFooter>
+        <Button>{children}</Button>
+      </CardFooter>
+    </form>
+  );
+};
+
+const LogSlug = ({ params }: { params: { slug: React.ReactNode } }) => {
+  return (
+    <Card className="m-4">
+      <LogForm>{params.slug}</LogForm>
+    </Card>
+  );
+};
+
+export default LogSlug;
--- src/components/log-page.tsx
+++ src/components/log-page.tsx
@@ -10,7 +10,12 @@ type LinkComponentProps = {
 
 const LinkComponent: React.FC<LinkComponentProps> = ({ href, children }) => {
   return (
-    <Link key={href} href={href} className="text-blue-500">
+    <Link
+      className="m-2 border-2"
+      key={href}
+      href={href}
+      className="text-blue-500"
+    >
       {children}
     </Link>
   );
@@ -18,14 +23,16 @@ const LinkComponent: React.FC<LinkComponentProps> = ({ href, children }) => {
 
 const LogPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full border-2 p-4 rounded">
-      <h2 className="text-2xl font-bold">
+    <div className="flex flex-col items-center justify-center w-[370px] h-full border-2 p-4 rounded">
+      <h2 className="text-2xl font-bold w-full flex justify-between items-center">
         {/* You can use one of React.FC's default attribute, 'children',
                     either as a 'prop' from self-closing <Link />-component
                      or you can also do it more 'old skool-ish' and so you
                      might destruct <LinkComponent> and use its 'tag'  */}
-        */
+
         <LinkComponent href={"/login-user"} children={"Kirjaudu sisään"} />
+
+        <div className="m-2" children={" | "} />
         <LinkComponent href={"/register-user"}>Rekisteröidy</LinkComponent>
       </h2>
     </div>
